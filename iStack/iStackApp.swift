722c2df1e0a91d0f79b8c8f26b5cab59a4f48c58
Stevanus Felixiano <stevanusf77@gmail.com>
2026-06-23 09:51:09 +0800

    Initial Commit

---
+++ iStack/iStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStackApp.swift
+//  iStack
+//
+//  Created by Stevanus Felixiano on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct iStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
